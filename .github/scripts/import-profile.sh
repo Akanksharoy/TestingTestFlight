b89bdcc2a4b8477c9b7ec6f805af2f8c4f40dceb
Akanksharoy <akanksha.roy95@gmail.com>
2020-12-19 19:58:45 +0530

    Update import-profile.sh

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+echo "$PROVISIONING_PROFILE_DATA"
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
